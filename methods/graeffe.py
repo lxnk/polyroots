19e46d946dde29ad0c4fa5079ff6fd56e2947b54
Alex Kashuba <o.kashuba@gmail.com>
2021-09-07 14:14:08 +0200

    Added limits estimation

--- methods/graeffe.py
+++ methods/graeffe.py
@@ -1,12 +1,60 @@
 # -*- coding: utf-8 -*-
 """Dandelin–Lobachesky–Graeffe method.
 https://en.wikipedia.org/wiki/Graeffe%27s_method
+
+Bounds on all roots
+https://en.wikipedia.org/wiki/Geometrical_properties_of_polynomial_roots#Bounds_on_all_roots
 """
 
 from . import *
 
 
+def _maxn(a: np.array, n: int = 1) -> np.array:
+    return a[np.argpartition(-a, n)[:n]]
+
+
+def _abs_norm(a: np.array) -> np.array:
+    return np.abs(a[:-1] / a[-1])
+
+
+def _abs_norm_dim(a: np.array) -> np.array:
+    b = _abs_norm(a)
+    return b ** (1 / np.arange(len(b), 0, -1))
+
+
+def _abs_ratio(a: np.array) -> np.array:
+    a = a[a != 0]
+    return np.abs(a[:-1] / a[1:])
+
+
+_root_limit_formula = {
+    "lagrange_weak": (lambda b: max(1, np.sum(b)), _abs_norm),
+    "cauchy": (lambda b: 1 + np.max(b), _abs_norm),
+    "zassenhaus": (lambda d: 2 * np.max(d), _abs_norm_dim),
+    # originally given by Lagrange, but attributed to Zassenhaus by Donald Knuth
+    "lagrange": (lambda d: np.sum(_maxn(d, 2)), _abs_norm_dim),
+    # Lagrange improved previous with formula using sum(max(_,2)) instead of 2*max(_)
+    "lagrange_ratio": (lambda c: np.sum(c), _abs_ratio),
+    "hoelder": (lambda b: np.sqrt(1 + np.sum(b ** 2)), _abs_norm),
+    "fujiwara": (lambda d: 2 * max(d[0] * 2 ** (-1 / (len(d))), np.max(d[1:])), _abs_norm_dim),
+    "kojima": (lambda c: 2 * max(c[0] / 2, np.max(c[1:])), _abs_ratio)
+    }
+
+
+def root_limit(p: Poly, method=None):
+    if method:
+        f = _root_limit_formula[method]
+        lim = f[0](f[1](p.coef))
+    else:
+        lim = np.inf
+        for m in _root_limit_formula:
+            f = _root_limit_formula[m]
+            lim = min(lim, f[0](f[1](p.coef)))
+    return lim
+
+
 # TODO: Estimate multiplicity
+
 
 def dgiteration(p: Poly):
     """Dandelin–Graeffe iteration
@@ -24,19 +72,29 @@ def dgiteration(p: Poly):
     New roots are at the larger distance
     """
     n = p.degree()
-    n2 = int((n+1)/2)
+    n2 = int((n + 1) / 2)
     m = np.ones_like(p.coef)
     m[1::2] = -1
     q = p.copy()
 
     for k in range(-n2, n + 1 - n2):
         if k >= 0:
-            q.coef[k] = np.sum(p.coef[:2*k+1] * p.coef[2*k::-1]
-                               * m[2*k::-1])
+            q.coef[k] = np.sum(p.coef[:2 * k + 1] * p.coef[2 * k::-1]
+                               * m[2 * k::-1])
         else:
-            q.coef[k] = np.sum(p.coef[2*k+1:] * p.coef[:2*k:-1]
-                               * m[:2*k:-1])
+            q.coef[k] = np.sum(p.coef[2 * k + 1:] * p.coef[:2 * k:-1]
+                               * m[:2 * k:-1])
     return q
+
+
+def estimate_roots(p: Poly):
+    i = 0
+    r = -p.coef[:-1] / p.coef[1:]
+    while np.any(r < 0):
+        p = dgiteration(p)
+        i += 1
+        r = -p.coef[:-1] / p.coef[1:]
+    return r**(2**(-i))
 
 
 def roots_classical(p: Poly, d: int):
@@ -47,8 +105,8 @@ def roots_classical(p: Poly, d: int):
     r = -q[-1].coef[:-1] / q[-1].coef[1:]
     # if __debug__:
     #     utils.show_roots(r, q[-1])
-    # print(r,'\n')
-    for i in range(d-1, -1, -1):
+    # print(r, '\n')
+    for i in range(d - 1, -1, -1):
         r = np.lib.scimath.sqrt(r)
         c = np.abs(q[i](-r)) < np.abs(q[i](r))
         # print(r)
@@ -62,11 +120,23 @@ def roots_classical(p: Poly, d: int):
 
 def roots_tangential(p: Poly, d: int, eps: float = 1e-5):
     # TODO: process with complex roots
-    pe = p+eps * p.deriv()
+    pe = p + eps * p.deriv()
     for i in range(d):
         p = dgiteration(p)
         pe = dgiteration(pe)
-    r = -p.coef[:-1]/p.coef[1:]
-    re = -pe.coef[:-1]/pe.coef[1:]
-    r = -2**d*eps*r/(re-r)
+    r = -p.coef[:-1] / p.coef[1:]
+    re = -pe.coef[:-1] / pe.coef[1:]
+    r = -2 ** d * eps * r / (re - r)
+    return r
+
+
+def roots_tangential(p: Poly, d: int, eps: float = 1e-5):
+    # TODO: process with complex roots
+    pe = p + eps * p.deriv()
+    for i in range(d):
+        p = dgiteration(p)
+        pe = dgiteration(pe)
+    r = -p.coef[:-1] / p.coef[1:]
+    re = -pe.coef[:-1] / pe.coef[1:]
+    r = -2 ** d * eps * r / (re - r)
     return r
--- tests/test_graeffe.py
+++ tests/test_graeffe.py
@@ -2,6 +2,7 @@
 
 from . import *
 from numpy.polynomial import polynomial as pl
+import numpy as np
 from methods import graeffe
 from utils import sort_roots
 
@@ -40,3 +41,32 @@ def test_roots_tangential(polyr):
     nt.assert_allclose(sort_roots(r), sort_roots(polyr.roots()), rtol=3e-2)
 
 
+@pytest.mark.parametrize("a,b,c,d", [([3, -7, 0, -1, 5],
+                                      [.6, 1.4, 0, .2],
+                                      [3 / 7, 7, 1 / 5],
+                                      [.6 ** (1 / 4), 1.4 ** (1 / 3), 0, .2]),
+                                     ([0, 4, -3, 0, 5, -2],
+                                      [0, 2, 1.5, 0, 2.5],
+                                      [4 / 3, 3 / 5, 5 / 2],
+                                      [0, 2 ** (1 / 4), 1.5 ** (1 / 3), 0, 2.5])], ids=["a5", "a6"])
+def test__abs_x(a, b, c, d):
+    nt.assert_array_equal(graeffe._abs_norm(np.array(a)), b)
+    nt.assert_array_equal(graeffe._abs_ratio(np.array(a)), c)
+    nt.assert_array_equal(graeffe._abs_norm_dim(np.array(a)), d)
+
+
+def test_root_limit(polyr):
+    maxabsr = np.max(np.abs(polyr.roots()))
+    for m in graeffe._root_limit_formula:
+        rlim = graeffe.root_limit(polyr, method=m)
+        nt.assert_array_less(maxabsr, rlim)
+
+
+def test_estimate_roots(polyr):
+    r = np.sort(graeffe.estimate_roots(polyr))
+    r0 = np.sort(np.abs(polyr.roots()))
+    print()
+    print(r0)
+    print(r, graeffe.root_limit(polyr, method="lagrange"))
+    nt.assert_allclose(r, r0, rtol=.5)
+    nt.assert_array_less(r, graeffe.root_limit(polyr, method="lagrange"))
