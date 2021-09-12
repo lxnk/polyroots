0f4f57e936dc9f36aef86d0444983f09118e5fed
Alex Kashuba <o.kashuba@gmail.com>
2021-09-12 23:50:09 +0200

    Sum implemented

--- methods/renormalization.py
+++ methods/renormalization.py
@@ -5,6 +5,27 @@ https://en.wikipedia.org/wiki/Graeffe%27s_method#Renormalization
 """
 
 import numpy as np
+import numpy.testing as nt
+
+
+def assert_array_almost_equal_nulp(x, y, nulp=1):
+    assert x.k == y.k
+    nt.assert_array_almost_equal_nulp(x.r, y.r, nulp=nulp)
+    nt.assert_array_almost_equal_nulp(x.p, y.p, nulp=nulp)
+
+
+def assert_array_almost_equal_nulp(x, y, nulp=1):
+    assert x.k == y.k
+    nt.assert_array_almost_equal_nulp(x.r, y.r, nulp=nulp)
+    nt.assert_array_almost_equal_nulp(x.p, y.p, nulp=nulp)
+
+
+def assert_allclose(actual, desired, rtol=1e-07, atol=0, equal_nan=True, err_msg='', verbose=True):
+    assert actual.k == desired.k
+    nt.assert_array_almost_equal_nulp(actual.r, desired.r, rtol=rtol, atol=atol, equal_nan=equal_nan, err_msg=err_msg,
+                                      verbose=verbose)
+    nt.assert_array_almost_equal_nulp(actual.p, desired.p, rtol=rtol, atol=atol, equal_nan=equal_nan, err_msg=err_msg,
+                                      verbose=verbose)
 
 
 class Rnumber:
@@ -18,20 +39,69 @@ class Rnumber:
     p: np.floating = 0
     k: np.integer = 0
 
-    def __init__(self, r: np.floating = 0, p: np.floating = 0, k: np.integer = 0):
-        self.r = r
-        self.p = p
-        self.k = k
+    # Unicode character mappings for improved __str__
+    _superscript_mapping = str.maketrans({
+        "0": "⁰",
+        "1": "¹",
+        "2": "²",
+        "3": "³",
+        "4": "⁴",
+        "5": "⁵",
+        "6": "⁶",
+        "7": "⁷",
+        "8": "⁸",
+        "9": "⁹"
+    })
+
+    def __init__(self, r: float = 0, p: float = 0, k: float = 0):
+        self.r = np.float64(r)
+        self.p = np.float64(p)
+        self.k = np.int32(k)
         self.p = self.p % 2
 
     def __repr__(self):
-        return "Rnumber"
+        return f"Rnum({self.r}, {self.p}, {self.k})"
+
+    def __format__(self, format_spec):
+        return f"2{str(self.k).translate(self._superscript_mapping)}⋅({self.r} + 𝜄𝜋⋅{self.p})"
+        # return f"2{self.k}⋅({self.r} + 𝜄𝜋⋅{self.p})"
 
     def __str__(self):
-        return f"2^{self.k} * ( {self.r} + pi * {self.p} )"
+        return f"2{str(self.k).translate(self._superscript_mapping)}⋅({self.r} + 𝜄𝜋⋅{self.p})"
+        # return f"2**{self.k} * ({self.r} + i*pi * {self.p} )"
+
+    @staticmethod
+    def _factor(k1: np.integer, k2: np.integer, dk: int = 0):
+        k = max(k1, k2) + dk
+        a = int(2 ** (k - k1))
+        b = int(2 ** (k - k2))
+        return k, a, b
 
     def __mul__(self, other):
-        k = max(self.k, other.k) + 1
-        a = 2**(self.k-k)
-        b = 2**(other.k-k)
-        return Rnumber(a*self.r + b*other.r, a*self.p + b*other.p, k)
+        k, a, b = self._factor(self.k, other.k, 1)
+        return Rnumber(self.r / a + other.r / b, self.p / a + other.p / b, k)
+
+    def _add_renorm(self, other, sub: bool = False):
+        k, a, b = self._factor(self.k, other.k, 0)
+        if sub:
+            dp = 1
+        else:
+            dp = 0
+        if self.r / a >= other.r / b:
+            c = other.r / b - self.r / a + 1j * np.pi * ((other.p + dp) / b - self.p / a)
+            r = self.r
+            p = self.p
+        else:
+            c = self.r / a - other.r / b + 1j * np.pi * (self.p / a - (other.p + dp) / b)
+            r = other.r
+            p = other.p
+        c = 1 + np.exp(2 ** k * c)
+        r += np.log(np.absolute(c))
+        p += np.angle(c) / np.pi
+        return Rnumber(r, p, k)
+
+    def __add__(self, other):
+        return self._add_renorm(other)
+
+    def __sub__(self, other):
+        return self._add_renorm(other, sub=True)
--- tests/test_renormalization.py
+++ tests/test_renormalization.py
@@ -1,12 +1,35 @@
 # -*- coding: utf-8 -*-
 
 from methods.renormalization import Rnumber
+from methods import renormalization as rn
+import pytest
+import numpy.testing as nt
+import numpy as np
 
 
 def test_rnumber():
+    c = Rnumber(1.2, 3.5)
+    print(type(c))
+    print(c)
+    print(f"R-number {c}")
+
+
+def test_mul():
+    c1 = Rnumber(1.2, 3.5)
+    c2 = Rnumber(0.6, 0.1)
+    # print(c1*c2)
+    rn.assert_array_almost_equal_nulp(c1 * c2, Rnumber(0.9, 0.8, 1))
+
+
+def test_sum():
     c1 = Rnumber(1.2, 3.5)
     c2 = Rnumber(0.6, 0.1)
     print()
-    print(c1*c2)
-    print(Rnumber(0.9, 0.8, 1))
-    assert c1*c2 == Rnumber(0.9, 0.8, 1)
+    print(c1+c2)
+
+
+def test_rray():
+    c1 = Rnumber(1.2, 3.5)
+    c2 = Rnumber(0.6, 0.1)
+    z = np.array([c1, c2])
+    print(z*z)
