15fcd4a71fbfcb9f3f13fd5bee725c24166e235e
Alex Kashuba <o.kashuba@gmail.com>
2021-10-05 16:52:28 +0200

    Decimation option

--- data/convert_npy2csv.py
+++ data/convert_npy2csv.py
@@ -2,15 +2,24 @@ import numpy as np
 
 x = np.zeros(11)
 
+# rng = np.random.default_rng(82749)
+
 with open('../data/polydata.npy', 'rb') as f:
     for d in range(2, 7):
         p = np.load(f)
-        print(f"{len(p['coef'])} polynomials of degree {d}")
+        pc = p['coef']
+        print(f"{len(pc)} polynomials of degree {d}")
+
+        if len(pc) > 128:
+            # print(rng.integers(low=0, high=len(p['coef']), size=128))
+            pc = pc[np.arange(0, len(pc), 9)]
+            print(f'take {len(pc)}')
 
         with open(f'poly{d}d.csv', 'w') as f2:
-            for pp in p['coef']:
+            for pp in pc:
                 x[:] = 0
                 x[0:d+1] = pp
+
                 s = np.array2string(x, max_line_width=np.inf, separator=',',
                                     formatter={'float_kind':lambda x: "%.14g" % x})
                 f2.write(f'{s};{d};// {np.max(x.nonzero())}th degree\n')
